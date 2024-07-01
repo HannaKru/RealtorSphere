a5c34f44c53d79c3b8b8d43ad7a7949bb4059030
MarinaShtei <amarinzg@gmail.com>
2024-07-01 20:44:45 +0300

    loginscreen2

--- firebase_config.py
+++ firebase_config.py
@@ -3,11 +3,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 def initialize_firebase():
-    # Initialize Firebase app
+    # Path to your Firebase service account key JSON file
     cred = credentials.Certificate("realtorsphere-34d53-firebase-adminsdk-psu5p-9c683ded43.json")
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://realtorsphere-34d53-default-rtdb.firebaseio.com/'
-    })
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
     return db
 
 db = initialize_firebase()
