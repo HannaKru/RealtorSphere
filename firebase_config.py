19124b7b02c3c77e4dcc688895e04e7c9232db8d
MarinaShtei <amarinzg@gmail.com>
2024-07-03 20:00:10 +0300

    Updated Firebase initialization to use environment variable

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+
 
 def initialize_firebase():
     if not firebase_admin._apps:
+        service_account_key_path = os.getenv('FIREBASE_KEY_PATH')
+
         # Path to your Firebase service account key JSON file
         cred = credentials.Certificate("realtorsphere-34d53-firebase-adminsdk-psu5p-9c683ded43.json")
         firebase_admin.initialize_app(cred)
