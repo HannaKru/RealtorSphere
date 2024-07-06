529021e6749b71a8205a6cedb5d6f0479ffdc2bf
MarinaShtei <amarinzg@gmail.com>
2024-07-06 19:26:50 +0300

    loginscreen and home page

--- firebase_config.py
+++ firebase_config.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import pyrebase
-
+#hi
 def initialize_firebase():
     cred = credentials.Certificate("static/realtorsphere-34d53-firebase-adminsdk-psu5p-c71cbe2996.json")
     firebase_admin.initialize_app(cred)
