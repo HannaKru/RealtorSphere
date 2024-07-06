c6a640ba9fa4f228a8350e52a6fb2ae8c565bab0
MarinaShtei <amarinzg@gmail.com>
2024-07-06 19:18:14 +0300

    loginscreen and home page

--- firebase_config.py
+++ firebase_config.py
@@ -3,14 +3,10 @@ from firebase_admin import credentials, firestore
 import pyrebase
 
 def initialize_firebase():
-    config = {
-        "apiKey": "AIzaSyCKz1AKMCqQjKyXTs98kWDNLj0Udr0EvjM",
-        "authDomain": "realtorsphere-34d53.firebaseapp.com",
-        "databaseURL": "https://realtorsphere-34d53-default-rtdb.firebaseio.com/",
-        "storageBucket": "gs://realtorsphere-34d53.appspot.com"
-    }
-    firebase = pyrebase.initialize_app(config)
-
-    db = firebase.database()
+    cred = credentials.Certificate("static/realtorsphere-34d53-firebase-adminsdk-psu5p-c71cbe2996.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
     return db
+
+
 db = initialize_firebase()
