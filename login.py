7abfe75b9fc39e6041dcfed092c3e5bb55cdd8f3
MarinaShtei <amarinzg@gmail.com>
2024-07-16 20:20:49 +0300

    registration screen

--- login.py
+++ login.py
@@ -4,13 +4,13 @@ db = initialize_firebase()
 
 def login_user(email, password):
     try:
-        users_ref = db.collection('Person')
-        query = users_ref.where('email', '==', email).stream()
-        for user in query:
-            user_data = user.to_dict()
-            # Check if the user is a Realtor and if the password matches
-            if 'Realtor' in user_data.get('Type', {}) and user_data['Type']['Realtor']['password'] == password:
-                return user_data
+        users_ref = db.child('Person')
+        users = users_ref.get()
+        for user_id, user_data in users.items():
+            if user_data.get('email') == email:
+                # Check if the user is a Realtor and if the password matches
+                if 'Realtor' in user_data.get('Type', {}) and user_data['Type']['Realtor']['password'] == password:
+                    return user_data
         return None
     except Exception as e:
         print(f"Error logging in: {e}")
