17094aba200c314a30c97aed7fdfa59a5d43b564
MarinaShtei <amarinzg@gmail.com>
2024-09-06 10:43:20 +0300

    added new price in deal option

--- firebase_config.py
+++ firebase_config.py
@@ -5,12 +5,12 @@ from firebase_admin import credentials, db, storage
 cred = credentials.Certificate("static/realtorspheredb-firebase-adminsdk-k43ko-1383b36823.json")
 default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://realtorspheredb-default-rtdb.firebaseio.com/',
-    'storageBucket': 'gs://realtorspheredb.appspot.com'
+    'storageBucket': '/realtorspheredb.appspot.com/'
 })
 
 def initialize_firebase():
     return db.reference()
 
 def get_storage_bucket():
-    bucket = storage.bucket('gs://realtorspheredb.appspot.com')
+    bucket = storage.bucket('realtorspheredb.appspot.com')
     return bucket
